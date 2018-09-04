8043fc15bf22e25643276ce9d9dd50384cf31a26
Gin2-lab <42832235+Gin2-lab@users.noreply.github.com>
2018-09-04 17:06:28 +0900

    Add files via upload

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -11,17 +11,40 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//各メンバーのページ
+import { MikiPage } from '../pages/miki/miki';
+import { KawaguchiPage } from '../pages/kawaguchi/kawaguchi';
+import { HasegawaPage } from '../pages/hasegawa/hasegawa';
+import { YaguchiPage } from '../pages/yaguchi/yaguchi';
+import { TaguchiPage } from '../pages/taguchi/taguchi';
+import { NishimuraPage } from '../pages/nishimura/nishimura';
+import { RyuPage } from '../pages/ryu/ryu';
+
+//HTTP接続部品
+import { GitHubUsersServiceProvider } from '../providers/git-hub-users-service/git-hub-users-service';
+import { HttpClientModule } from '@angular/common/http'; 
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+
+    //各メンバーのページ宣言
+    MikiPage,
+    KawaguchiPage,
+    HasegawaPage,
+    YaguchiPage,
+    TaguchiPage,
+    NishimuraPage,
+    RyuPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +52,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+
+    //各人のページエントリ
+    MikiPage,
+    KawaguchiPage,
+    HasegawaPage,
+    YaguchiPage,
+    TaguchiPage,
+    NishimuraPage,
+    RyuPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GitHubUsersServiceProvider
   ]
 })
 export class AppModule {}
