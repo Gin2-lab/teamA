f505eca0aada50716bfd9f0a69146dcec6073d39
Gin2-lab <42832235+Gin2-lab@users.noreply.github.com>
2018-09-05 11:44:16 +0900

    sample_yaguchi
    upload sample by yaguchi

---
+++ src/pages/example3/example3.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Example3Page } from './example3';
+
+@NgModule({
+  declarations: [
+    Example3Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Example3Page),
+  ],
+})
+export class Example3PageModule {}
